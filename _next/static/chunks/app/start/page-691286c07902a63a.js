1829d4e45034584cafeea1251df3632daf054fc3
Alen <49744385+Cpio1@users.noreply.github.com>
2026-09-11 22:51:56 +0500

    Update page-691286c07902a63a.js

--- _next/static/chunks/app/start/page-691286c07902a63a.js
+++ _next/static/chunks/app/start/page-691286c07902a63a.js
@@ -4,7 +4,7 @@
 			"use strict";r.d(t, {
 				w: () => s
 			});
-			let n = ["Ivan Ivanov", "Yerkenur Zhanabekova", "Amina Kurmanzhan", "Diyar Saken", "Klimentiy Li", "Abdu Askhat", "Zhanaiym Korpebayeva", "Grigory Razygraev", "Mikhail Razygraev", "Kirill Vozianov", "Zhangirkhan Kozhin"];
+			let n = ["Ivan Ivanov", "Yerkenur Zhanabekova", "Amina Kurmanzhan", "Diyar Saken", "Klimentiy Li", "Abdu Askhat", "Zhanaiym Korpebayeva", "Grigory Razygraev", "Mikhail Razygraev", "Kirill Vozianov", "Zhangirkhan Kozhin", "Yerassyl Mustafa"];
 
 			function s(e, t) {
 				if(!e || !t) return !1;
